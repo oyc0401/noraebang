e0edb9feb136d813fe970aae67feaa8a3ae814cf
yuchan <oyc0401@gmail.com>
2026-01-02 00:48:10 +0900

    프론트 오류 수정

--- frontend/src/app/channel/[alias]/page.tsx
+++ frontend/src/app/channel/[alias]/page.tsx
@@ -1,25 +1,24 @@
 import { notFound } from "next/navigation";
 import { artistsControllerFindByIdOrAlias } from "@/api/model/artists/artists";
 import { songsControllerFindAll } from "@/api/model/songs/songs";
-import { getResponseData } from "@/api/utils";
-import type { Artist, Song } from "@/types/models";
+import type { ArtistDto, SongDto } from "@/api/model/models";
 import ArtistPageClient from "./ArtistPageClient";
 
-async function getArtist(alias: string): Promise<Artist> {
+async function getArtist(alias: string): Promise<ArtistDto> {
   try {
     const response = await artistsControllerFindByIdOrAlias(alias);
-    return getResponseData<Artist>(response);
+    return response.data;
   } catch {
     return notFound();
   }
 }
 
-async function getSongs(artistId: number): Promise<Song[]> {
+async function getSongs(artistId: number): Promise<SongDto[]> {
   try {
     const response = await songsControllerFindAll({
       artistId: artistId.toString(),
     });
-    return getResponseData<Song[]>(response);
+    return response.data;
   } catch {
     return [];
   }
