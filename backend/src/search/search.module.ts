827e025865b7a9a0e93de0cfacb5c8b88a842ee8
yuchan <oyc0401@gmail.com>
2026-01-05 02:09:45 +0900

    검색 외부 의존성 제거

--- backend/src/search/search.module.ts
+++ backend/src/search/search.module.ts
@@ -1,11 +1,9 @@
 import { Module } from "@nestjs/common";
-import { ArtistsModule } from "../artists/artists.module";
-import { SongsModule } from "../songs/songs.module";
 import { SearchController } from "./search.controller";
 import { SearchService } from "./search.service";
 
 @Module({
-  imports: [ArtistsModule, SongsModule],
+  imports: [],
   controllers: [SearchController],
   providers: [SearchService],
   exports: [SearchService],
