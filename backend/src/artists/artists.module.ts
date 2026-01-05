35393ce0fae991fd202c7345c4172d56105229d2
yuchan <oyc0401@gmail.com>
2026-01-06 06:35:42 +0900

    유튜브 모듈 제거

--- backend/src/artists/artists.module.ts
+++ backend/src/artists/artists.module.ts
@@ -1,11 +1,10 @@
 import { Module } from "@nestjs/common";
 import { PrismaModule } from "../prisma/prisma.module";
-import { YoutubeModule } from "../youtube/youtube.module";
 import { ArtistsController } from "./artists.controller";
 import { ArtistsService } from "./artists.service";
 
 @Module({
-  imports: [PrismaModule, YoutubeModule],
+  imports: [PrismaModule],
   controllers: [ArtistsController],
   providers: [ArtistsService],
   exports: [ArtistsService],
--- backend/src/youtube/dto/youtube-channel-update.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from "@nestjs/swagger";
-
-export class YoutubeChannelUpdateDto {
-  @ApiProperty({
-    description: "YouTube 채널 ID 또는 @handle",
-    example: "UCvpredjG93ifbCP1Y77JyFA",
-  })
-  channelId!: string;
-}
