dee65c456f4608b2e6175defdeae5a0d3a959596
yuchan <oyc0401@gmail.com>
2026-01-09 04:09:12 +0900

    타입센스 적용

--- backend/src/search/search.module.ts
+++ backend/src/search/search.module.ts
@@ -1,9 +1,10 @@
 import { Module } from "@nestjs/common";
+import { TypesenseModule } from "../typesense/typesense.module";
 import { SearchController } from "./search.controller";
 import { SearchService } from "./search.service";
 
 @Module({
-  imports: [],
+  imports: [TypesenseModule],
   controllers: [SearchController],
   providers: [SearchService],
   exports: [SearchService],
