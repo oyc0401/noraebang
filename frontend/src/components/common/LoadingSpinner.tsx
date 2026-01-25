1709db711bc7c9ecf54c97f718c1cddf695a734b
yuchan <oyc0401@gmail.com>
2026-01-25 19:13:51 +0900

    검색창에 붙여넣기 섹션 추가

--- frontend/src/components/common/LoadingSpinner.tsx
+++
@@ -1,5 +0,0 @@
-export const LoadingSpinner = () => (
-  <div className="flex justify-center items-center p-8">
-    <div className="animate-spin rounded-full h-12 w-12 border-4 border-zinc-800 border-t-white" />
-  </div>
-);
